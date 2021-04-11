73fb1ed63f6cd2bf949e5b039206030ee6bdb2d4
Jinpeng.zhou <jinpeng.zhou@my.jcu.edu.au>
2021-04-11 16:33:12 +0800

    Add the menu

---
+++ assignment1.py
@@ -0,0 +1,23 @@
+"""
+Replace the contents of this module docstring with your own details
+Name:Jinpeng Zhou
+Date started:11/04/2021
+GitHub URL:https://github.com/JCUS-CP1404/assignment-1-reading-tracker-KampangChauJCU.git
+"""
+
+
+MENU = """Menu:
+L - List all books
+A - Add new book
+W - Mark a book as completed
+Q - Quit
+"""
+
+
+def main():
+    """..."""
+    print("Reading Tracker 1.0 - by Your Name")
+
+
+if __name__ == '__main__':
+    main()
